ff741badf4382e54d3ab4b2081503ab854e8c88e
Gregor <gregor@martynus.net>
2017-12-10 00:35:03 -0800

    test: check for travis job number to run cron only once

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
 require('./smoke')
 require('./validate')
 
-if (process.env.TRAVIS_EVENT_TYPE === 'cron') {
-  console.log('🤖  Looking for schema updates ...')
+if (process.env.TRAVIS_EVENT_TYPE === 'cron' &&
+    process.env.TRAVIS_JOB_NUMBER &&
+    process.env.TRAVIS_JOB_NUMBER.split('.')[1] === '1') {
+  console.log('🤖  Looking for schema updates …')
   require('../bin/download')
 }
