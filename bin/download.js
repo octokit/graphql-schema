1ecc7cdece1747f4cc0572c2359a60c4f1519f49
Gregor <gregor@martynus.net>
2019-03-24 11:23:39 -0700

    refactor: use @octokit/request and execa

--- bin/download.js
+++ bin/download.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
 const writeFileSync = require('fs').writeFileSync
-const gitDiff = require('git-diff')
-const axios = require('axios')
 
 require('dotenv').config()
 
@@ -11,45 +9,46 @@ if (!process.env.GH_TOKEN) {
   process.exit(1)
 }
 
+const execa = require('execa')
+const request = require('@octokit/request').defaults({
+  headers: {
+    authorization: `bearer ${process.env.GH_TOKEN}`
+  }
+})
+
 const oldSchemaIdl = require('../').schema.idl
 let newSchema = {
   json: null,
   idl: null
 }
 
-console.log('⌛  Loading GitHub GraphQL schema …')
-Promise.all([
-  axios.get('https://api.github.com/graphql', {
-    headers: {
-      Authorization: `bearer ${process.env.GH_TOKEN}`
+console.log('⌛  Loading GitHub GraphQL JSON schema …')
+request('https://api.github.com/graphql')
+
+  .then(response => {
+    writeFileSync('schema.json', JSON.stringify(newSchema.json, null, 2))
+
+    console.log('⌛  Loading GitHub GraphQL IDL schema …')
+    return request('https://api.github.com/graphql', {
+      headers: {
+        accept: 'application/vnd.github.v4.idl'
+      }
+    })
+  })
+
+  .then(async response => {
+    writeFileSync('schema.graphql', response.data.data)
+
+    const { stdout } = await execa('git', ['status', 'schema.graphql'])
+    if (/nothing to commit/.test(stdout)) {
+      console.log(`✅  Schema is up-to-date`)
+      return
     }
-  }),
-  axios.get('https://api.github.com/graphql', {
-    headers: {
-      Accept: 'application/vnd.github.v4.idl',
-      Authorization: `bearer ${process.env.GH_TOKEN}`
+
+    console.log(`📼  New schema recorded`)
+
+    if (process.env.TRAVIS_EVENT_TYPE === 'cron') {
+      console.log('🤖  Fixture changes detected in cron job. Creating pull request ...')
+      require('./create-pull-request')
     }
   })
-])
-
-.then(responses => {
-  newSchema.json = responses[0].data.data
-  newSchema.idl = responses[1].data.data
-  const diff = gitDiff(oldSchemaIdl, newSchema.idl)
-
-  if (!diff) {
-    console.log(`✅  Schema is up-to-date`)
-    return
-  }
-
-  writeFileSync('schema.json', JSON.stringify(newSchema.json, null, 2))
-  writeFileSync('schema.graphql', newSchema.idl)
-  console.log(`📼  New schema recorded`)
-
-  if (process.env.TRAVIS_EVENT_TYPE === 'cron') {
-    console.log('🤖  Fixture changes detected in cron job. Creating pull request ...')
-    require('./create-pull-request')
-  }
-})
-
-.catch(console.log)
