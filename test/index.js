9a472fa6b5ec61b5a0712520d284a0f1878284e3
Gregor <gregor@martynus.net>
2019-03-24 10:26:48 -0700

    ci: update on travis using CRON stage

--- test/index.js
+++ test/index.js
@@ -1,9 +1,2 @@
 require('./smoke')
 require('./validate')
-
-if (process.env.TRAVIS_EVENT_TYPE === 'cron' &&
-    process.env.TRAVIS_JOB_NUMBER &&
-    process.env.TRAVIS_JOB_NUMBER.split('.')[1] === '1') {
-  console.log('🤖  Looking for schema updates …')
-  require('../bin/download')
-}
